826ff43dd7395ce58d8f2698d83d2c18cbf1c583
Aomaru.S <84952318+Aomaru-S@users.noreply.github.com>
2024-09-08 17:01:54 +0000

    Initialize the appearance of the web page

---
+++ app/src/components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from "@/components/footer.module.css"
+
+export default function Footer() {
+  return (
+    <footer className={ styles.footer }>
+      © Aomaru.S
+    </footer>
+  )
+}
---
+++ app/src/components/header.tsx
@@ -0,0 +1,9 @@
+import styles from "@/components/header.module.css"
+
+export default function Header() {
+  return (
+    <header className={ styles.header }>
+      Aoi-Ryo.com
+    </header>
+  )
+}
